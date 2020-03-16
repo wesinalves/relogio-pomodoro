bbf12c80567d0c1b7be87f76a1e8c17bb17916e1
wesin ribeiro <wesinec@gmail.com>
2020-03-16 19:44:07 -0300

    add relogio component

---
+++ src/app/relogio/relogio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relogio',
+  templateUrl: './relogio.component.html',
+  styleUrls: ['./relogio.component.scss']
+})
+export class RelogioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
