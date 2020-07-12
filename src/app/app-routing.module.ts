2742668a3eb88d763a3f9ad5a8a743d23ad8404b
WesinAlves <wesinec@gmail.com>
2020-07-12 20:50:16 -0300

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { RelogioComponent } from './relogio/relogio.component';
+import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
+import { PaginaDuvidasComponent } from './pagina-duvidas/pagina-duvidas.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: RelogioComponent, pathMatch: 'full' },
+    {path: 'duvidas', component: PaginaDuvidasComponent},
+    {path: 'config', component: ConfiguracoesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
